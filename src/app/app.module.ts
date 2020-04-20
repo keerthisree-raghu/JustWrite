530ae35b6962b97da174d5977677ea03a949e216
Keerthisree <raghu.keerthisree@gmail.com>
2020-04-20 13:05:43 +0530

    Preliminary front-end completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatExpansionModule} from '@angular/material/expansion';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
     selector: 'app-post-list',
     templateUrl: './post-list.component.html',
     styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent {
-    posts = [
-        {title: 'First Post', content: 'First Content'},
-        {title: 'First Post', content: 'First Content'},
-        {title: 'First Post', content: 'First Content'},
-    ];
+export class PostListComponent implements OnInit, OnDestroy {
+    // posts = [
+    //     {title: 'First Post', content: 'First Content'},
+    //     {title: 'First Post', content: 'First Content'},
+    //     {title: 'First Post', content: 'First Content'},
+    // ];
+
+    posts: Post[] = [];
+    private postsSub: Subscription;
+
+    constructor(public postsService: PostsService) {}
+
+    ngOnInit() {
+        this.posts = this.postsService.getPosts();
+        this.postsSub = this.postsService.getPostUpdateListener()
+            .subscribe((posts: Post[]) => {
+                this.posts = posts;
+            });
+    }
+
+    ngOnDestroy() {
+        this.postsSub.unsubscribe();
+    }
 }
