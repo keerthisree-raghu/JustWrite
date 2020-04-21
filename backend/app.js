b1cd33c4c75f9191f74bea3c7ff8623cd9c6458e
Keerthisree <raghu.keerthisree@gmail.com>
2020-04-21 19:08:50 +0530

    Connection to MongoDB database completed

--- backend/app.js
+++ backend/app.js
@@ -1,50 +1,65 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const Post = require("./models/post");
 
 const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://keerthi:6zOjR277h2siURRi@justwrite-cluster-ot9en.mongodb.net/just-write?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Headers", 
-        "Origin, X-Requested-With, Content-Type, Accept"
-        );
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PATCH, DELETE, OPTIONS"
-        );
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
 });
 
-// Post Method
 app.post("/api/posts", (req, res, next) => {
-    const post = req.body;
-    console.log(post);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save().then(createdPost => {
     res.status(201).json({
-        message: "Post added successfully"
+      message: "Post added successfully",
+      postId: createdPost._id
     });
+  });
 });
 
 app.get("/api/posts", (req, res, next) => {
-    const posts = [
-        {
-            id: "afsfskj2345",
-            title: "First server-side post",
-            content: "This is from the server"
-        },
-        {
-            id: "fkgjalw2234",
-            title: "First server-side post",
-            content: "This is from the server"
-        }
-    ];
+  Post.find().then(documents => {
     res.status(200).json({
-        message: "Posts fetched successfully!",
-        posts: posts
+      message: "Posts fetched successfully!",
+      posts: documents
     });
+  });
+});
+
+app.delete("/api/posts/:id", (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then(result => {
+    console.log(result);
+    res.status(200).json({ message: "Post deleted!" });
+  });
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const normalizePort = val => {
   var port = parseInt(val, 10);
 
   if (isNaN(port)) {
-    // Named Pipe
+    // named pipe
     return val;
   }
 
   if (port >= 0) {
-    // Port Number
+    // port number
     return port;
   }
 
@@ -22,7 +22,7 @@ const onError = error => {
   if (error.syscall !== "listen") {
     throw error;
   }
-  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
   switch (error.code) {
     case "EACCES":
       console.error(bind + " requires elevated privileges");
@@ -39,7 +39,7 @@ const onError = error => {
 
 const onListening = () => {
   const addr = server.address();
-  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
   debug("Listening on " + bind);
 };
 
