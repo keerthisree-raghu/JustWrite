59ed6bf4d1301fcf4d35266c889f0b1853d8f27d
Keerthisree <raghu.keerthisree@gmail.com>
2020-04-21 07:53:10 +0530

    Backend code added

---
+++ backend/app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers", 
+        "Origin, X-Requested-With, Content-Type, Accept"
+        );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, DELETE, OPTIONS"
+        );
+    next();
+});
+
+// Post Method
+app.post("/api/posts", (req, res, next) => {
+    const post = req.body;
+    console.log(post);
+    res.status(201).json({
+        message: "Post added successfully"
+    });
+});
+
+app.get("/api/posts", (req, res, next) => {
+    const posts = [
+        {
+            id: "afsfskj2345",
+            title: "First server-side post",
+            content: "This is from the server"
+        },
+        {
+            id: "fkgjalw2234",
+            title: "First server-side post",
+            content: "This is from the server"
+        }
+    ];
+    res.status(200).json({
+        message: "Posts fetched successfully!",
+        posts: posts
+    });
+});
+
+module.exports = app;
